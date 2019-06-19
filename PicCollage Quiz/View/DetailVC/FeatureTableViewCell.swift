53efaca83a19112e4b3934f19e7184a7ffa69d43
Jo Hsu <jaune.yh@gmail.com>
2019-06-19 23:59:11 +0800

    Add info and feature cell

---
+++ PicCollage Quiz/View/DetailVC/FeatureTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  FeatureTableViewCell.swift
+//  PicCollage Quiz
+//
+//  Created by Jo Yun Hsu on 2019/6/19.
+//  Copyright © 2019 Jo Yun Hsu. All rights reserved.
+//
+
+import UIKit
+
+class FeatureTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
